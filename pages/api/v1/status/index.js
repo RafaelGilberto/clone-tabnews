0b11676ffeaf0fd244cc34468e8eafc9cb655e9c
Rafael Gilberto <rgrl516@gmail.com>
2026-04-23 00:41:57 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,14 +3,30 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
-  console.log(databaseVersionResult);
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectsResult = await database.query(
+    "SHOW max_connections;",
+  );
+
+  const databaseMaxConnectsValue =
+    databaseMaxConnectsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectsValue),
+        opened_connections: databaseOpenedConnectionsValue,
       },
     },
   });
